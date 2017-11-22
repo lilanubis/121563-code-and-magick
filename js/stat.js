c5e40a3fcc556e1a671001efbab614af45ca8f54
Olga Baryshnikova <vasiek1@yandex.ru>
2017-11-22 18:17:47 +0100

    refactoring

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,47 @@
 'use strict';
 
+var RECT_WIDTH = 40;
+var STEP = 50;
+var STARTING_X = 160;
+var STARTING_Y = 240;
+var STARTING_Y_NAMES = 260;
+var MAX_HEIGHT = 150;
+
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+
+  function drawStatisticsBoard() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(110, 20, 420, 270);
+    ctx.fillStyle = 'white';
+    ctx.strokeRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270);
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
+  }
+
+  drawStatisticsBoard();
 
   var max = Math.max.apply(null, times);
-  var maxHeight = 150;
-  var scale = maxHeight / max;
-  var rectWidth = 40;
-  var step = 50;
-  var startingX = 160;
-  var startingY = 240;
-  var startingYnames = 260;
+  var scale = MAX_HEIGHT / max;
 
   for (var i = 0; i < names.length; i++) {
-
     var rectHeight = scale * -times[i];
-    var closingX = startingX + ((rectWidth + step) * i);
-    var closingY = startingY + rectHeight;
+    var closingX = STARTING_X + ((RECT_WIDTH + STEP) * i);
+    var closingY = STARTING_Y + rectHeight;
     var startingYtimes = closingY - 10;
 
     ctx.strokeStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + (Math.random() * (1 - 0.2) + 0.2).toFixed(1) + ')';
 
     ctx.beginPath();
-    ctx.moveTo(closingX, startingY);
+    ctx.moveTo(closingX, STARTING_Y);
     ctx.lineTo(closingX, closingY);
-    ctx.lineWidth = rectWidth;
+    ctx.lineWidth = RECT_WIDTH;
     ctx.stroke();
 
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), (startingX + ((rectWidth + step) * i)) - 20, startingYtimes);
-    ctx.fillText(names[i], (startingX + ((rectWidth + step) * i)) - 20, startingYnames);
+    ctx.fillText(Math.round(times[i]), closingX - 20, startingYtimes);
+    ctx.fillText(names[i], closingX - 20, STARTING_Y_NAMES);
   }
 };
